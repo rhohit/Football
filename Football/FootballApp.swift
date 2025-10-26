626cf4bf4df86ced88420ba5c5947817a9295904
rhohit <rhohita@gmail.com>
2025-10-26 09:53:39 +0100

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by ro on 10/26/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
